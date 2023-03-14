0bc2adcc50ee64a45a31196578998a5c7dee35c1
Eli <e.m.mills1995@gmail.com>
2023-03-14 13:31:33 -0400

    Set up initial framework.

---
+++ chat_server.py
@@ -0,0 +1,41 @@
+def set_up_socket():
+    # 1. Create socket object
+    # 2. Bind socket to given port
+    # 3. Return socket
+    pass
+
+
+def read_incoming_data():
+    # 1. Receive data length
+    # 2. Receive rest of data
+    # 3. Print data
+    pass
+
+
+def handle_user_input():
+    # 1. Prompt for input
+    # 2. Parse input
+    # 3. Return data, or stop signal if /q
+    pass
+
+
+def send_outgoing_data():
+    # 1. Format and package data
+    # 2. Send data
+    pass
+
+
+def main():
+    # 1. Set up socket
+    # 2. Main loop
+    #       2.1. Listen for connection
+    #       2.2. Handle incoming data
+    #       2.3. Handle user input
+    #       2.4. Check for connection terminator
+    #       2.5. Handle outgoing data
+    # 3. Close socket
+    pass
+
+
+if __name__ == '__main__':
+    main()
