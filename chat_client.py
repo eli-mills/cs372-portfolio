91c48c015f36510506ff936ad88929f703f1e37f
Eli <e.m.mills1995@gmail.com>
2023-03-22 15:26:29 -0400

    Refactor so chatter uses single function .chat() to either read or send.

--- chat_client.py
+++ chat_client.py
@@ -6,16 +6,11 @@ def main(host, port):
     # Set up socket
     with socket.create_connection((host, port)) as server_socket:
         print(f"Connected to: {host} on port: {port}")
-        do_long_prompt = True
         chatter = ChatSocket(server_socket)
 
         # Main loop
-        while True:
-            if chatter.send_user_input(do_long_prompt):
-                break
-            do_long_prompt = False
-            if chatter.read_and_print():
-                break
+        while chatter.state != ChatSocket.TERMINATE:
+            chatter.chat()
 
 
 if __name__ == '__main__':
--- chat_server.py
+++ chat_server.py
@@ -1,14 +1,17 @@
 import socket
 import argparse
+import tic_tac_toe
 
 
 class ChatSocket:
-    CHATTING, PLAYING = range(2)
+    CHATTING, WAITING, TERMINATE = range(3)
 
-    def __init__(self, conn_socket):
+    def __init__(self, conn_socket, is_server=False):
         self.conn_socket = conn_socket
         self.SOCKET_BUFFER = 1024  # Buffer argument for recv
-        self.state = self.CHATTING
+        self.state = self.WAITING if is_server else self.CHATTING
+        self.do_long_prompt = True
+        self.game = None
 
     @staticmethod
     def parse_socket_data(socket_content: str) -> list[str]:
@@ -48,30 +51,50 @@ class ChatSocket:
         while total_bytes < len(byte_msg):
             total_bytes += self.conn_socket.send(byte_msg[total_bytes:])
 
-    @staticmethod
-    def parse_for_command(message: str, prints: bool = True) -> bool:
+    def make_player_move(self, move):
+        pass
+
+    def start_game(self):
+        self.game = tic_tac_toe.TicTacToeGame()
+
+    def parse_for_command(self, message: str, prints: bool = True) -> None:
         """
         Checks if message is a command. Returns whether to stop main loop.
         :param message: string to parse
         :param prints: whether to print the message if not a command
-        :return: True if need to stop main loop, else False
         """
         if message == "/q":
-            return True
+            self.state = self.TERMINATE
+            return
         if message == "/tic":
-            return False
+            pass
         prints and print(message)
-        return False
 
-    def read_and_print(self):
+    def read_and_handle(self) -> bool:
+        """
+        Handle incoming message from socket.
+        :return: True if message should break main loop, else False.
+        """
         message_received = self.read_incoming_data()
-        return self.parse_for_command(message_received)
+        self.state = self.CHATTING
+        self.parse_for_command(message_received)
 
-    def send_user_input(self, do_long_prompt):
-        do_long_prompt and print("Type /q to quit\nEnter message to send...")
+    def send_user_input(self):
+        self.do_long_prompt and print("Type /q to quit\nEnter message to send...")
         new_message = input(">")
         self.send_outgoing_data(new_message)
-        return self.parse_for_command(new_message, False)
+        self.state = self.WAITING
+        self.parse_for_command(new_message, False)
+        self.do_long_prompt = False
+
+    def chat(self):
+        """
+        Waits for incoming message or sends new message depending on state.
+        """
+        if self.state == self.CHATTING:
+            self.send_user_input()
+        elif self.state == self.WAITING:
+            self.read_and_handle()
 
 
 def get_args(desc=""):
@@ -93,16 +116,11 @@ def main(host, port):
         with client_socket:
             print(f"Connected by {addr}")
             print("Waiting for message...")
-            do_long_prompt = True
-            chatter = ChatSocket(client_socket)
+            chatter = ChatSocket(client_socket, True)
 
             # Main loop
-            while True:
-                if chatter.read_and_print():
-                    break
-                if chatter.send_user_input(do_long_prompt):
-                    break
-                do_long_prompt = False
+            while chatter.state != ChatSocket.TERMINATE:
+                chatter.chat()
 
 
 if __name__ == '__main__':
